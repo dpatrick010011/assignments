cf4718cfb51e96c1290127cce1193e5065037b2c
Dane Patricks <48688653+dpatrick010011@users.noreply.github.com>
2019-04-22 14:29:19 -0600

    Add files via upload

---
+++ exercises/OddEven.js
@@ -0,0 +1,13 @@
+const counter = function () {
+  for (i = 0; i <= 100; i++){
+    if (i%2===1){
+      console.log('odd')
+    }else{
+      console.log('even') 
+    }
+
+    
+    }
+    
+  }
+  counter()
